b326f2d293c8ddc9ba8be5621fd499be480a699c
Shamsan-Nasir <shamsannsr@gmail.com>
2026-09-17 14:24:11 +0600

    workinng  on sign out

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logoMain from '../assets/icons8-compass-50 (1).png'
 import './Footer.css'
+import { AuthContext } from '../Authentication/AuthProvider'
 export const NavBar = () => {
+
+    let { user } = useContext(AuthContext)
     return (
         <div className="navbar bg-compass-sand shadow-sm p-2 sm:p-3 pr-3 sm:pr-5 ">
 
@@ -35,18 +38,18 @@ export const NavBar = () => {
                                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                         </div>
                     </div>
-                    <ul
-                        tabIndex={-1}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
+                    {
+                        user && <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                </a>
+                            </li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    }
                 </div>
             </div>
         </div>
